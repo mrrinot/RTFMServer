2bc90ce7febd827ce059cd07d213e96293f5b58f
Raphael (depuis son fixe) <raphael.ricard@epitech.eu>
2017-10-04 21:26:58 +0200

    dummy server for items

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 "use strict";
 
-require("./src/tools/confSetup");
-require("./src/app");
+// require("./src/tools/confSetup");
+// require("./src/app");
+const app = require("express")();
+
+app.get("/items/:input", (req, res) => {
+  res.json([
+    {
+      input: req.params.input,
+      id: 6994,
+      name: "Cape Hôte",
+      iconId: 17080,
+      level: 56,
+      description:
+        "Cette création hybride entre une hôte et une cape, est franchement étonnante. Si elle avait été confectionnée en cuir, le Captain Chafer l'aurait adorée.",
+    },
+  ]);
+});
+
+app.listen(8080, () => {
+  console.log("server running !");
+});
